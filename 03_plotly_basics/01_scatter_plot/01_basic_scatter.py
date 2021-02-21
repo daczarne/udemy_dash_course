317431933efaa5ca0dac11a8e854f00ea05a6025
Daniel Czarnievicz <daniel.czarnievicz@gmail.com>
2021-02-21 19:11:49 -0300

    basic scatter

---
+++ 03_plotly_basics/01_scatter_plot/01_basic_scatter.py
@@ -0,0 +1,21 @@
+# Import libraries and modules
+import numpy as np
+import plotly.offline as pyo
+import plotly.graph_objs as go
+
+# Generate random data
+np.random.seed(42)
+random_x = np.random.randint(1, 101, 100)
+random_y = np.random.randint(1, 101, 100)
+
+# Build a plotly object
+data = [
+    go.Scatter(
+        x = random_x,
+        y = random_y,
+        mode = "markers"
+    )
+]
+
+# Save the object
+pyo.plot(data, filename = "01_basic_scatter.html")
