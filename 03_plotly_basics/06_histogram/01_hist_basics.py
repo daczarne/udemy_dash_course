d26546cbd678683245e0813798a32609c58f1197
Daniel Czarnievicz <daniel.czarnievicz@gmail.com>
2021-02-22 00:31:36 -0300

    hist basics

--- 03_plotly_basics/06_histogram/01_hist_basics.py
+++ 03_plotly_basics/06_histogram/01_hist_basics.py
@@ -1,17 +1,27 @@
 # Import libraries and modules
 import plotly.offline as pyo
 import plotly.graph_objs as go
+import pandas as pd
 
 # Define data array
-
+df = pd.read_csv("mpg.csv", na_values = {'horsepower':'?'})
 
 # Create the traces
+data = [
+  go.Histogram(
+    x = df["mpg"]
+  )
+]
 
-
+# Define the layout
+layout = go.Layout(
+  title = "MPG"
+)
 
 # Build the figure
 fig = go.Figure(
-  data = data
+  data = data,
+  layout = layout
 )
 
 # Save the plot
