d5abfee826963b3be83d63c66b563089d624d874
Daniel Czarnievicz <daniel.czarnievicz@gmail.com>
2021-02-21 21:33:41 -0300

    dodged bars

---
+++ 03_plotly_basics/03_bar_chart/02_dodged_bars.py
@@ -0,0 +1,50 @@
+# Import libraries and modules
+import pandas as pd
+import plotly.offline as pyo
+import plotly.graph_objs as go
+
+# Read data
+df = pd.read_csv("2018WinterOlympics.csv")
+
+# Build the traces
+trace_0 = go.Bar(
+  x = df["NOC"],
+  y = df["Gold"],
+  name = "Gold",
+  marker = dict(
+    color = "#ffd700"
+  )
+)
+
+trace_1 = go.Bar(
+  x = df["NOC"],
+  y = df["Silver"],
+  name = "Silver",
+  marker = dict(
+    color = "#9ea0a1"
+  )
+)
+
+trace_2 = go.Bar(
+  x = df["NOC"],
+  y = df["Bronze"],
+  name = "Bronze",
+  marker = dict(
+    color = "#cd7f32"
+  )
+)
+
+data = [trace_0, trace_1, trace_2]
+
+layout = go.Layout(
+  title = "Total medals"
+)
+
+# Build the plot
+fig = go.Figure(
+  data = data,
+  layout = layout
+)
+
+# Save the plot
+pyo.plot(fig, filename = "02_dodged_bars.html")
