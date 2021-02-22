64b073d10f491a98916425ba857b6935bd745eea
Daniel Czarnievicz <daniel.czarnievicz@gmail.com>
2021-02-21 23:58:28 -0300

    boxplot basics

---
+++ 03_plotly_basics/05_box_plot/01_boxplot_basics.py
@@ -0,0 +1,22 @@
+# Import libraries and modules
+import plotly.offline as pyo
+import plotly.graph_objs as go
+
+# Define data array
+y = [1, 14, 14, 15, 16, 18, 18, 19, 19, 20,
+     20, 23, 24, 26, 27, 27, 28, 29, 33, 54]
+
+# Create the traces
+data = [
+  go.Box(
+    y = y
+  )
+]
+
+# Build the figure
+fig = go.Figure(
+  data = data
+)
+
+# Save the plot
+pyo.plot(fig, filename = "01_boxplot_basics.html")
