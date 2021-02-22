10f0ebe0fede25e89134573bef3c71f9a95f6eab
Daniel Czarnievicz <daniel.czarnievicz@gmail.com>
2021-02-21 22:52:21 -0300

    exercise

---
+++ 03_plotly_basics/03_bar_chart/04_exercise.py
@@ -0,0 +1,39 @@
+#######
+# Objective: Create a stacked bar chart from
+# the file mocksurvey.csv. Note that questions appear in
+# the index (and should be used for the x-axis), while responses
+# appear as column labels.  Extra Credit: make a horizontal bar chart!
+######
+
+# Import libraries and modules
+import pandas as pd
+import plotly.offline as pyo
+import plotly.graph_objs as go
+
+# Read the data into a pandas df
+df = pd.read_csv("mocksurvey.csv", index_col = 0)
+
+# Create the traces
+data = [
+  go.Bar(
+    x = df.index,
+    y = df[response],
+    name = response
+  )
+  for response in df.columns
+]
+
+# Create the layout
+layout = go.Layout(
+  title = "Survey Results",
+  barmode = "stack"
+)
+
+# Build the figure
+fig = go.Figure(
+  data = data,
+  layout = layout
+)
+
+# Save the plot
+pyo.plot(fig, filename = "04_exercise.html")
