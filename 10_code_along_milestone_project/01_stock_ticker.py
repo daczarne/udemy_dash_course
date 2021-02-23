db5e0349692de2edbea47afb7a5213f764b80b18
Daniel Czarnievicz <daniel.czarnievicz@gmail.com>
2021-02-23 11:39:01 -0300

    basic layout

---
+++ 10_code_along_milestone_project/01_stock_ticker.py
@@ -0,0 +1,31 @@
+import dash
+import dash_html_components as html
+import dash_core_components as dcc
+from dash.dependencies import Input, Output, State
+
+# Instanciate the app
+app = dash.Dash()
+
+# Build the layout
+app.layout = html.Div(
+  [
+    html.H1("Stock Ticker Dashboard"),
+    html.H3("Enter a stock symbol"),
+    html.Input(
+      id = "stock_picker",
+      values = "TSLA"
+    ),
+    gcc.Graph(
+      id = "graph",
+      figure = {
+        "data": [
+          {"x": [1, 2], "y": [3, 1]}
+        ]
+      }
+    )
+  ]
+)
+
+# Run the app
+if __name__ == "__main__":
+  app.run_server(debug = True)
